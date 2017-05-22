7d04542d7d92750a491b0a1500d60a4e73526de1
lfang615 <lfang615@users.noreply.github.com>
2017-05-22 17:09:07 -0500

    Add files via upload

---
+++ calculator/calculator/CalculatorLibrary.js
@@ -0,0 +1,41 @@
+﻿function numberClick() {
+    txtInput.value = txtInput.value == '0' ?
+            this.innerText : txtInput.value + this.innerText;
+}
+
+function plusClick() {
+    txtResult.value = Number(txtResult.value) + Number(txtInput.value);
+ 
+}
+
+function minusClick() {
+    txtResult.value = Number(txtResult.value) - Number(txtInput.value);
+  
+}
+
+document.getElementById('btn5').addEventListener('click', numberClick, false);
+
+var txtInput;
+var txtResult;
+
+function initialize() {
+    for (var i = 0; i < 10; i++) {
+        document.getElementById('btn' + i).addEventListener('click', numberClick, false);
+    }
+    txtInput = document.getElementById('txtInput');
+    txtResult = document.getElementById('txtResult');
+
+    document.getElementById('btnPlus').addEventListener('click', plusClick, false);
+    document.getElementById('btnMinus').addEventListener('click', minusClick, false);
+    document.getElementById('btnClearEntry').addEventListener('click', clearEntry, false);
+    document.getElementById('btnClear').addEventListener('click', clear, false);
+}
+
+function clearEntry() {
+    txtInput.value = '0';
+}
+
+function clear() {
+    txtIinput.value = '0';
+    txtResult.value = '0';
+}
---
+++ calculator/calculator/Scripts/MainScript.js
@@ -0,0 +1,82 @@
+﻿function numberClick() {
+    txtInput.value = txtInput.value == '0' ?
+            this.innerText : txtInput.value + this.innerText;
+}
+
+function plusClick() {
+    if (txtResult.value == 0) {
+        txtResult.value = Number(txtInput.value);
+    }
+    else {
+        //txtResult.value = Number(txtResult.value) + Number(txtInput.value);
+        txtResult = txtResult + '+';
+    }
+
+    clearEntry();
+
+}
+
+function minusClick() {
+    txtResult.value = Number(txtResult.value) - Number(txtInput.value);
+    clearEntry();
+}
+
+function multiplyClick() {
+    if (txtResult.value == 0) {
+        txtResult.value = Number(txtResult).value + Number(txtInput.value);
+    }
+    else {
+        txtResult.value = Number(txtResult.value) * Number(txtInput.value);
+    }
+  
+    clearEntry();
+}
+
+function divideClick() {
+    if (Number(txtResult.value) == 0) {
+        txtResult.value = Number(txtResult).value + Number(txtInput.value);
+    }
+    else {
+        txtResult.value = Number(txtResult.value) / Number(txtInput.value);
+    }
+   
+    clearEntry();
+}
+
+function equalsClick() {
+  var result = txtResult + txtInput;
+  txtResult.value = eval(result);
+}
+
+
+
+var txtInput = 0;
+var txtResult = 0;
+
+
+
+function clearEntry() {
+    txtInput.value = '0';
+}
+
+function clear() {
+    txtInput.value = '0';
+    txtResult.value = '0';
+}
+
+function initialize() {
+    for (var i = 0; i < 10; i++) {
+        document.getElementById('btn' + i).addEventListener('click', numberClick, false);
+    }
+    txtInput = document.getElementById('txtInput');
+    txtResult = document.getElementById('txtResult');
+
+    document.getElementById('btnPlus').addEventListener('click', plusClick, false);
+    document.getElementById('btnMinus').addEventListener('click', minusClick, false);
+    document.getElementById('btnMultiply').addEventListener('click', multiplyClick, false);
+    document.getElementById('btnDivide').addEventListener('click', divideClick, false);
+    document.getElementById('btnClearEntry').addEventListener('click', clearEntry, false);
+    document.getElementById('btnClear').addEventListener('click', clear, false);
+    document.getElementById('btnEquals').addEventListener('click', equalsClick, false);
+ 
+}
